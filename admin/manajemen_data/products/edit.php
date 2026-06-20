1316c345eee86c309191fb638999efacc0a4d33b
vincentvalk <vincentvaleriankoo@itbss.ac.id>
2026-06-20 23:44:19 +0700

    edit sidebar

--- admin/manajemen_data/products/edit.php
+++ admin/manajemen_data/products/edit.php
@@ -40,10 +40,10 @@ if (!$data) {
             <li>
                 <a href="javascript:void(0)" class="dropdown-btn">📁 Master Data <span class="arrow">▼</span></a>
                 <ul class="dropdown-container">
-                    <li><a href="master_data/profil_lokasi.php">🏢 Profil & Lokasi</a></li>
-                    <li><a href="master_data/visi_misi.php">🎯 Visi & Misi</a></li>
-                    <li><a href="master_data/keunggulan.php">⭐ Keunggulan</a></li>
-                    <li><a href="master_data/purchase_info.php">🛒 Info Pembelian</a></li>
+                    <li><a href="master_data/profil_lokasi/index.php">🏢 Profil & Lokasi</a></li>
+                    <li><a href="master_data/visi_misi/index.php">🎯 Visi & Misi</a></li>
+                    <li><a href="master_data/keunggulan/index.php">⭐ Keunggulan</a></li>
+                    <li><a href="master_data/info_pembelian/index.php">🛒 Info Pembelian</a></li>
                 </ul>
             </li>
 
