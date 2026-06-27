fb6db16f3b8594186cf88f4868cdce42fef090b3
vincentvalk <vincentvaleriankoo@itbss.ac.id>
2026-06-27 19:11:58 +0700

    implementasi code tambahan keamanan website

--- koneksi.php
+++ koneksi.php
@@ -9,4 +9,8 @@ $koneksi = mysqli_connect($host, $user, $pass, $db);
 if (!$koneksi) {
     die("Yahh, koneksi ke database gagal: " . mysqli_connect_error());
 }
+
+ini_set('session.cookie_httponly', 1);
+ini_set('session.use_only_cookies', 1);
+ini_set('session.cookie_samesite', 'Strict');
 ?>
